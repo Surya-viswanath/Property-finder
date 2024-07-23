8d2be9d65d7a6417784696530b237b2d3ecc5f68
Surya-viswanath <121759302+Surya-viswanath@users.noreply.github.com>
2024-07-23 20:18:12 +0530

    Update Rent.jsx

--- frontend/finder/src/components/Rent.jsx
+++ frontend/finder/src/components/Rent.jsx
@@ -18,7 +18,7 @@ function Rent() {
   useEffect(() => {
     const handleItems = async () => {
       try {
-        const response = await axios.get('http://localhost:4008/getpro');
+        const response = await axios.get('https://property-finder-backend.onrender.com/getpro');
         setFirst(response.data);
       } catch (error) {
         console.error('Error fetching data:', error);
