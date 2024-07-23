d5a5f5b257f51efbc9977c94e801f875dddcf74f
Surya-viswanath <121759302+Surya-viswanath@users.noreply.github.com>
2024-07-23 19:42:53 +0530

    Update Adminsignup.jsx

--- frontend/finder/src/Admin/Adminsignup.jsx
+++ frontend/finder/src/Admin/Adminsignup.jsx
@@ -28,7 +28,7 @@ const handlename =(event)=>{
    
       event.preventDefault()
       try{
-      const display =await axios.post('http://localhost:4008/adminsign',{name,email,phone,password})
+      const display =await axios.post('https://property-finder-backend.onrender.com/adminsign',{name,email,phone,password})
       console.log(display.data)}
       catch{ 
       }
